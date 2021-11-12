e3a7345a67defad9f9fbe397f633c538ca8716fe
Giadaspt <saputo.giada@gmail.com>
2021-11-12 14:02:02 +0100

    stile completato

---
+++ milestone1/js/script.js
@@ -0,0 +1,31 @@
+
+const root = new Vue({
+
+  el: '#root',
+
+  data: {
+
+    logo: 'boolean',
+    button: 'Aggiungi alla lista',
+
+    Obj: [
+      {
+        text: '',
+        done: false,
+      }
+    ],
+
+
+
+
+  },
+
+
+
+
+
+
+
+
+
+});
