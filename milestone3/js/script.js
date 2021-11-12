14f1ef20c7ba0978934d544e388cc47e12b19e4b
Giadaspt <saputo.giada@gmail.com>
2021-11-12 16:12:38 +0100

    milestone4

---
+++ milestone3/js/script.js
@@ -0,0 +1,42 @@
+/*
+  - quando viene cliccata la x l'elemento viene eliminato dall'array
+*/
+
+const root = new Vue({
+
+  el: '#root',
+
+  data: {
+
+    logo: 'boolean',
+    button: 'Aggiungi alla lista',
+
+    obj: [
+      {
+        text: 'Cose da fare oggi',
+        done: false,
+      },
+      {
+        text: 'Cose da fare domani',
+        done: false,
+      },
+      {
+        text: 'Cose da fare dopodomani',
+        done: false,
+      },
+      {
+        text: 'Cose da fare in giornata',
+        done: false,
+      },
+    ],
+  },
+
+  methods: {
+
+    removeOnClick(index){
+      this.obj.splice(index,1)
+      console.log(index);
+    }
+  },
+
+});
